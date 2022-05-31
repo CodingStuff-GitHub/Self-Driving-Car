b5b34fece8868aeaf6ee2e3583ab5023c97cd27d
CodingStuff-Github <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-05-31 13:30:03 +0530

    Starter Files

---
+++ main.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('canvas');
+//Canvas Height and Width
+canvas.width = window.innerWidth;
+canvas.height = 200;
+//Get context to draw on canvas
+const ctx = canvas.getContext('2d');
